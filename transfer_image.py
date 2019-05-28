f013f7821c2044eedd79a1106787541781ef714d
grepto <grepto@gmail.com>
2019-05-28 19:18:46 +0300

    Split big func upload_images_to_instagram t oseveral one-functionality funcs. Small refactoring. Grouping constants

--- transfer_image.py
+++ transfer_image.py
@@ -5,6 +5,9 @@ import requests
 
 load_dotenv()
 IMAGE_DIRECTORY = os.getenv('IMAGE_DIRECTORY')
+POSTED_IMAGES_FILE = os.getenv('POSTED_IMAGES_FILE')
+INSTAGRAM_LOGIN = os.getenv('INSTAGRAM_LOGIN')
+INSTAGRAM_PASSWORD = os.getenv('INSTAGRAM_PASSWORD')
 
 if not os.path.exists(IMAGE_DIRECTORY):
     raise NotADirectoryError(f'Image directory {IMAGE_DIRECTORY} not fount')
@@ -19,33 +22,36 @@ def download_image(image_url, image_name):
         file.write(response.content)
 
 
-def upload_images_to_instagram():
-    load_dotenv()
-    LOGIN = os.getenv('INSTAGRAM_LOGIN')
-    PASSWORD = os.getenv('INSTAGRAM_PASSWORD')
-    POSTED_IMAGES_FILE = 'posted_images.txt'
-
+def post_image_to_instagram(image_name, caption=''):
     bot = Bot()
-    bot.login(username=LOGIN, password=PASSWORD)
+    bot.login(username=INSTAGRAM_LOGIN, password=INSTAGRAM_PASSWORD)
+    image_file_name = os.path.join(IMAGE_DIRECTORY, image_name)
+    bot.upload_photo(image_file_name, caption=caption)
 
-    def post_image_to_instagram(image_name, caption=''):
-        image_file_name = os.path.join(IMAGE_DIRECTORY, image_name)
-        bot.upload_photo(image_file_name, caption=caption)
 
+def get_posted_images():
     try:
-        with open(POSTED_IMAGES_FILE, 'r', encoding='utf8') as f:
-            posted_images = f.read().splitlines()
+        with open(POSTED_IMAGES_FILE, 'r', encoding='utf8') as file:
+            return file.read().splitlines()
     except FileNotFoundError:
-        posted_images = []
+        return []
 
+
+def add_posted_images(posted_images):
+    with open(POSTED_IMAGES_FILE, 'w', encoding='utf8') as f:
+        for image in posted_images:
+            f.write(image + '\n')
+
+
+def upload_images_to_instagram():
     image_files = os.listdir(IMAGE_DIRECTORY)
+    posted_images = get_posted_images()
     for image in image_files:
         if image not in posted_images:
             caption = os.path.splitext(image)[0]
             post_image_to_instagram(image, caption=caption)
             posted_images.append(image)
+    add_posted_images(posted_images)
 
-    with open(POSTED_IMAGES_FILE, 'w', encoding='utf8') as f:
-        for image in posted_images:
-            f.write(image + '\n')
+
 
