d660509ef04e122aa6a43185e5179d0b87df5c1e
grepto <grepto@gmail.com>
2019-05-28 19:48:43 +0300

    clear comments and debug prints

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from transfer_image import upload_images_to_instagram
 
 if __name__ == '__main__':
     print('Download images from spacex')
-    # fetch_spacex_last_launch()
-    #
+    fetch_spacex_last_launch()
+
     print('Download images from hubble')
-    # collection_name = 'spacecraft'
-    # fetch_hubble_collection()
+    collection_name = 'spacecraft'
+    fetch_hubble_collection()
 
     print('Upload images to instagram')
     upload_images_to_instagram()
